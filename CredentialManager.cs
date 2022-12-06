eaba2948d173acb965207c72f0bd34df1cc181ff
Prithvi79pa <67819949+Prithvi79pa@users.noreply.github.com>
2022-12-06 14:57:22 +0530

    MY SDK Allegion

---
+++ CredentialManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allegion_SDK
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CredentialManager
+    {
+        private int x;
+
+        /// <value>Property <c>X</c> represents the point's x-coordinate.</value>
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public void CreateCredential()
+        {
+            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uname"></param>
+        /// <param name="pass"></param>
+        /// <param name="apiID"></param>
+        /// <returns></returns>
+        public int getCredentials(string uname, string pass, int apiID)
+        {
+            return 199;
+
+        }
+
+        public int getCredentials2(string uname, string pass, int apiID)
+        { 
+            if (true)
+            {
+                throw new Exception("invalid");
+            }
+            else
+            {
+                return 100;
+            }
+
+        }
+
+    }
+}
