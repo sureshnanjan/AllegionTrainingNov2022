e6bdb81fb46c64de46ed54a39632f4a3fcf22b8f
Arindam Kashyap <akashyap4@allegion.com>
2022-12-07 11:53:27 +0530

    Checked the Selenium WEB Driver

--- AllegionTrainingNov2022/Program.cs
+++ AllegionTrainingNov2022/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace AllegionTrainingNov2022
 {
     internal class Program
@@ -16,11 +17,27 @@ namespace AllegionTrainingNov2022
             {
                 Console.WriteLine("Success");
             }
-            else { Console.WriteLine("Failure"); }
-
-
-
-
+            else 
+            { 
+                Console.WriteLine("Failure"); 
+            }
+            ChromeDriver _browser = new ChromeDriver();
+            _browser.Url = "https://the-internet.herokuapp.com";
+            Console.WriteLine(_browser.Title);
+            IWebElement headingElement = _browser.FindElement(By.TagName("H1"));
+            string actulalHEadingText = headingElement.Text;
+            if (actulalHEadingText.Equals("Welcome to the-internet"))
+            {
+                Console.WriteLine("Yes, Test is successed");
+            }
+            else
+            {
+                Console.WriteLine("No, Its a failure");
+            }
+            _browser.Close();
+            // headingElement.Text
+            // Console.WriteLine(headingElement.Text);
+            // _browser.Close();
         }
     }
 }
