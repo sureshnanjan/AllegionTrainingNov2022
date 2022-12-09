a3d7e28bedd6e3fa6f2c2f095bfade733684d133
Arindam Kashyap <akashyap4@allegion.com>
2022-12-09 23:43:10 +0530

    Program.cs added in MyAllegionUtility

---
+++ MyAllegionUtility/Programcs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAllegionUtility
+{
+    internal class Programcs
+    {
+        private string astring; 
+        static void Main()
+        {
+            TestString objectOne = new TestString("mama");
+            TestString objectTwo = new TestString("niku");
+            //objectOne[0];
+
+            Console.WriteLine(objectOne + objectTwo);       
+        }
+    }
+
+    internal class TestString
+    {
+        public string astring;
+
+        public TestString()
+        {
+        }
+
+        public TestString(string astring)
+        {
+            this.astring = astring;
+        }
+
+        public static TestString operator +(TestString objectOne, TestString objectTwo)
+        {
+            TestString temp = new TestString();
+            temp.astring = (objectOne.astring + objectTwo.astring);
+            return temp;
+        }
+    }
+}
